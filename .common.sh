98ef240d882d5b71f172c0ed3a3a3fb8c489d7cc
Vardhan Mudunuru <vardhan@vardhan-macbookpro2.roam.corp.google.com>
2018-03-26 02:16:01 -0700

    intro: sr, update: .tmux.conf

--- .common.sh
+++ .common.sh
@@ -1,7 +1,7 @@
-alias ta='tmux attach'
-
 # ctrl+s in VIM hangs.  To disable:
 stty -ixon
+
+alias ta='tmux attach';
 
 # for grepping through specific types of files:
 #   s <CSV of file extensions> <text to search>
@@ -15,15 +15,39 @@ function s {
   grep "${=incl}"
 }
 
+function sr {
+  # is this a dry run?
+  dry_run="";
+  if [ "$1" == "-i" ] ; then
+    dry_run="-i.bak";
+    shift;
+  fi
+
+  # which files to include
+  incl="";
+  for x in "${1/,/ /}"; do
+    incl="$incl --include '*$x'";
+  done;
+  shift;
+
+  # the search and replace text
+  tofind=$1;
+  toreplace=$2;
+  cmd="grep ${incl} -l -R -e ${tofind} .";
+  eval $cmd | xargs sed $dry_run "s/${tofind}/${toreplace}/g";
+}
+
 # for finding a file by its name
 function o { find -name "$@" }
 
-export EDITOR="vim"
-export TERM="xterm-256color"
+export EDITOR="vim";
+export TERM="xterm-256color";
 
-export GOPATH="$HOME/go"
-export PATH=$PATH:$GOPATH
+export GOPATH="$HOME/go";
+export PATH=$PATH:$GOPATH;
 
-alias n="ninja -C out/Debug -j 1000"
+alias n="ninja -C out/Debug -j 1000";
 
-[[ ( -f $HOME/.google.sh ) ]] && source $HOME/.google.sh
+if [[ -f $HOME/.google.sh ]]; then
+  source $HOME/.google.sh
+fi
